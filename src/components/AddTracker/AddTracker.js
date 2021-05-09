2b1e89de03b6012295f1e7a1bb0b1c35f69da4aa
SerdzhyoVashchuk <serj.vaschuk@gmail.com>
2021-05-09 21:45:35 +0300

    add 4  Icon (svg) commponets && style for TrackerAdd and title

--- src/components/AddTracker/AddTracker.js
+++ src/components/AddTracker/AddTracker.js
@@ -1,16 +1,21 @@
 import Button from '../Button';
 import s from './AddTracker.module.scss';
+import IconPlayCircle from '../IconPlayCircle';
 
 const AddTracker = () => {
   return (
     <form className={s.form}>
-      <input
-        type="text"
-        className={s.input}
-        placeholder="Enter tracker name"
-        value=""
-      ></input>
-      <Button></Button>
+      <div className={s.inputWrapper}>
+        <input
+          type="text"
+          className={s.input}
+          placeholder="Enter tracker name"
+          value=""
+        />
+        <Button className={s.btn}>
+          <IconPlayCircle />
+        </Button>
+      </div>
     </form>
   );
 };
